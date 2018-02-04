706bc4829b5bcfbf5a561acfc6793ddc4ae86af3
xubo245 <601450868@qq.com>
2018-02-04 21:57:25 +0800

    add sparkLearning 2

--- SparkLearning1.5/src/main/scala/org/apache/spark/avro/learning/testPrint1.scala
+++
@@ -1,7 +0,0 @@
-package org.apache.spark.avro.learning
-
-object testPrint1 {
-  def main(args: Array[String]): Unit = {
-    println("hello");
-  }
-}
--- SparkLearning1.5/src/main/scala/org/apache/spark/ml/test/TestMl.scala
+++
@@ -1,5 +0,0 @@
-package org.apache.spark.ml.test
-
-object TestMl {
-
-}
